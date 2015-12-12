ebaac271935ec8082a0a4a3900e0fdfcb467f173
Niko Wenselowski <der@nik0.de>
2015-12-12 18:35:33 +0100

    Second challenge.

--- tests/test_day12.py
+++ tests/test_day12.py
@@ -13,3 +13,10 @@ def test_counting():
 
     assert 0 == count_numbers("[]")
     assert 0 == count_numbers("{}")
+
+
+def test_counting_and_ignoring_red():
+    assert 6 == count_numbers('[1,2,3]', ignore_red=True)
+    assert 4 == count_numbers('[1,{"c":"red","b":2},3]', ignore_red=True)
+    assert 0 == count_numbers('{"d":"red","e":[1,2,3,4],"f":5}', ignore_red=True)
+    assert 6 == count_numbers('[1,"red",5]', ignore_red=True)
